1cfc67c4f4278097c219af59856d7949f2b092e2
RidusM <stormkillpeople@gmail.com>
2022-12-07 15:44:26 +0500

    ss2

--- database.py
+++ database.py
@@ -18,14 +18,6 @@ def table_create_staff(id: int, name: str):
         print("Ошибка2")
         return 'error'
 
-def table_create_projects_cost(id: int, project_name: str):
-    try:
-        cursor.execute("INSERT INTO Costs(id, project_name) VALUES (?,?)", (id, project_name))
-        conn.commit()
-    except sqlite3.Error as Er:
-        print("Ошибка3")
-        return 'error'
-
 def table_update_staff_cost(id: str, hour_cost: int):
     try:
         cursor.execute("UPDATE staff SET hour_cost = ? where id = ?", (id, hour_cost))
@@ -34,21 +26,21 @@ def table_update_staff_cost(id: str, hour_cost: int):
         print("Ошибка4")
         return 'error'
 
-def table_update_projects_cost(id: int, week_cost: int):
+def table_update_projects_cost(id: int, cost: int):
     try:
-        cursor.execute("UPDATE Costs SET week_cost =? where id =?", (id, week_cost))
+        cursor.execute("UPDATE Projects SET cost =? where id =?", (id, cost))
         conn.commit()
     except sqlite3.Error as Er:
         print("Ошибка5")
         return 'error'
 
 def db_table_selectAll():
-    cursor.execute("SELECT * FROM Costs")
+    cursor.execute("SELECT * FROM Projects")
     row = cursor.fetchone()
     out = []
     while row is not None:
         i=1
-        out.append(f"""\n        <tr><td>{i}</td> \n<td align="center">%s</td>""" % row[2])
-        out.append("""\n        <td><228></td><td><228></td><td align="center">%s</td>\n<td><228></td>\n<td><228></td></tr>""" % row[3])
+        out.append(f"""\n        <tr><td>{i}</td> \n<td align="center">%s</td>""" % row[1])
+        out.append("""\n        <td><228></td><td><228></td><td align="center">%s</td>\n<td><228></td>\n<td><228></td></tr>""" % row[2])
         row = cursor.fetchone()
     return ''.join(out)
--- reader.py
+++ reader.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+
 import requests
 import database as db
 
@@ -20,14 +23,29 @@ def project_costs_reader():
     data = response.json()
     entries = data['response']['items']
     for item in entries:
-        db.table_create_projects_cost(id=item['id'], project_name=item['name'])
+        db.table_create_projects(id=item['id'], project_name=item['name'])
 
 def tasks_reader():
-    for page in range(1,30):
+    project_sum_task = 0;
+    project_sum_time = 0;
+    for page in range(1,20):
         print(page)
         response = requests.get(f'https://byuropink.aspro.cloud/api/v1/module/task/tasks/list?api_key=WkN0a1hhQWc5NG5JNks3elFjZ1FYZlZpc2lLMHIyRVhfODgwOTY&page={page}')
         data = response.json()
         entries = data['response']['items']
         for item in entries:
-           emae = item['model_id'], item['id'], item['name'], item['responsible_id'], item['plan_start_date'],  item['time_estimate']
-           print(emae)
+            if item['plan_start_date'] == '0000-00-00 00:00:00':
+                item['plan_start_date'] = None
+            pn_st_dt = item['plan_start_date']
+            pn_st_dt = datetime.strptime(pn_st_dt, '%Y-%m-%d %H:%M:%S')
+            print(datetime.now().isocalendar()[1])
+            print(pn_st_dt)
+            if item['model_id'] == 8 &  pn_st_dt.isocalendar()[1] == f'{datetime.now().isocalendar()[1]}':
+                print('yes model')
+                project_sum_task += 1
+                if item['time_estimate'] > 0:
+                    project_sum_time += item['time_estimate']
+                    print('yes_time')
+            emae = item['model_id'], item['id'], item['name'], item['responsible_id'], item['plan_start_date'],  item['time_estimate']
+    print(project_sum_task)
+    print(project_sum_time)
